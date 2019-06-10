ee6d38d82a69f41688abc83cbd0d260752181472
alicia1206 <Student@1913518-C18093.lanecc.edu>
2019-06-10 16:06:19 -0700

    new ttt-react repo!

---
+++ src/App.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor (props) {
+    super(props);
+    // state-related instance variables
+    this.state = {};
+    this.state.squares = Array(9).fill(null);
+    this.state.xIsNext = true;
+    this.state.winner = null;
+    this.state.winningLine = [];
+
+    // do not want this to render, so it is not part of state
+    this.lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+        ];
+    //this.calculateWinner = this.calculateWinner.bind(this);
+    this.handleClick = this.handleClick.bind(this); 
+  }
+
+  render() {
+    let status;
+    // if there is a winner contained in state
+    //  status = "Winner:" + the winner contained in state
+    // else
+    //  status = "Next player:" + the next player
+    if (this.state.winner) {
+      status = 'Winner: ' + this.state.winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+    // return the status and the current board
+    // calls renderSquare method to create squares on board
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+            {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+            {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+            {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+  // renderSquare method updates a square on the board
+  renderSquare(i) {
+    const className = (this.state.squares[i] == null) ? "square" :
+        (this.state.winner != null && this.state.winner === this.state.squares[i]
+          && this.state.winningLine.includes(i)) ? 
+        "square-winner" : "square-full";
+    const enabled = (this.state.winner == null && this.state.squares[i] == null) ? true : false;
+    const eventHandler = (enabled)? this.handleClick: () => {};
+    const output = (
+        <div className={className} id={i}
+            onClick={eventHandler}>
+            {(this.state.squares[i] != null) ? this.state.squares[i] : ""}
+        </div>
+    ); 
+    return output;
+  }
+  // event handler that calculates winner and updates state
+  handleClick(event) {
+    console.log(event.target.id);
+    const i = event.target.id;
+    let squares = Object.assign({}, this.state.squares);
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    const theWinner = this.calculateWinner(squares);
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+      winner: theWinner.player,
+      winningLine: theWinner.winningLine
+    });    
+  } 
+  // this calculates the winner using squares as parameter
+  // returns a winning player and a winning line
+  calculateWinner(squares) 
+  {
+    for (let i = 0; i < this.lines.length; i++) {
+        const [a, b, c] = this.lines[i];       
+        if (squares[a] && 
+        squares[a] === squares[b] && 
+        squares[a] === squares[c]) {
+          return {player: squares[a], winningLine: this.lines[i]};
+        }
+    }
+    return {player: null, winningLine: []};    
+  }
+}
+export default App;
